9ad516eecae98bd728dfc7786cd67e126f88b82e
Shubhendra Patel <patelshubhendra@gmail.com>
2024-03-28 00:55:03 +0530

    plans and subscription history updated

--- UIBase_DoNotAlter/findhouse/src/pages/api/topUpPaypalPayment.js
+++ UIBase_DoNotAlter/findhouse/src/pages/api/topUpPaypalPayment.js
@@ -1,44 +1,49 @@
 import axios from "axios";
 import CryptoJS from "crypto-js";
-import withSession from "../../utils/session/session";
 
-async function handler(request, response) {
-  const decryptionKey = process.env.CRYPTO_SECRET_KEY;
-  const domain = process.env.BACKEND_DOMAIN;
+
+ async function handler (request,response) {
+
+    const decryptionKey = process.env.CRYPTO_SECRET_KEY;
+    const domain = process.env.BACKEND_DOMAIN;
 
   try {
+
     const encryptedBody = await request.body.data;
 
     const decryptedBytes = CryptoJS.AES.decrypt(encryptedBody, decryptionKey);
     const body = JSON.parse(decryptedBytes.toString(CryptoJS.enc.Utf8));
+   
 
-  console.log(request);
-  return ;
-    const userResponse = await axios.post(`${domain}/FileUpload/fileupload`, {
-      email: email,
-      password: password,
-    });
-    const user = userResponse.data;
+    const {userId,planId,token} = body;
+   
 
-    if (!user) {
-      return response.status(404).json({ error: "User Not Found" });
-    }
-    return response.status(200).json({ msg: "OK", userData: user });
+    const userResponse = await axios.post(`${domain}/com.appraisalland.Plan/getTopUpPlan?Planid=${planId}&UserId=${userId}`,
+    null,{
+        headers: {
+          Authorization:`Bearer ${token}`
+        }
+      });
+    const users = userResponse.data;
+
+
+    return response.status(200).json({msg:"OK",data : users});
   } catch (err) {
-   console.log(err);
+    
     if (err.response) {
+      console.log(err.response)
       // If the error is from an axios request (e.g., HTTP 4xx or 5xx error)
       const axiosError = err.response.data;
       const statusCode = err.response.status;
-      console.log(axiosError);
-      console.error(statusCode, axiosError.message); // Log the error for debugging
+      console.error(statusCode,axiosError.message); // Log the error for debugging
 
       return response.status(statusCode).json({ error: axiosError.message });
     } else {
       // Handle other types of errors
       return response.status(500).json({ error: "Internal Server Error" });
     }
+
   }
 }
-
+ 
 export default handler;
